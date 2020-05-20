cdd58acbac9aadc1ca0c46e5c8a594f8441664f7
Junhao "Arthur" Chen <junhaochenarthur@outlook.com>
2020-05-19 22:19:24 -0400

    firebase pod init

---
+++ Zion/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  Zion
+//
+//  Created by Junhao Chen on 5/19/20.
+//  Copyright © 2020 Rose-Hulman. All rights reserved.
+//
+
+import Foundation
+
+class Article : ContentEntry{
+    
+}
