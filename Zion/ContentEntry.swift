ef8ef1dfc71d0198ecc1d9ed45b6c688feb99a8e
Junhao "Arthur" Chen <junhaochenarthur@outlook.com>
2020-05-19 22:13:53 -0400

    Add some model file and basic UI

---
+++ Zion/ContentEntry.swift
@@ -0,0 +1,16 @@
+//
+//  ContentEntry.swift
+//  Zion
+//
+//  Created by Junhao Chen on 5/19/20.
+//  Copyright © 2020 Rose-Hulman. All rights reserved.
+//
+
+import Foundation
+
+class ContentEntry {
+    
+    func getTitle() -> String {
+        return "Title"
+    }
+}
